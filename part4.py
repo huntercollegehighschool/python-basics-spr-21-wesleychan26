a3204f1312d28ce3334c121cb127a615b2dceac1
Wesley Chan <>
2021-04-14 23:51:59 +0000

    I corrected codes 1 through 3 and wrote codes for 4 and 5.

--- part4.py
+++ part4.py
@@ -14,3 +14,6 @@ The area of the triangle is 12.0
 '''
 
 #start writing your code below
+base = int(input("Enter a base: "))
+height = int(input("Enter a height: "))
+print("The area of this triangle is", base * height/2)
--- part5.py
+++ part5.py
@@ -13,3 +13,6 @@ Hello, Ash Ketchum
 '''
 
 #start writing your code below
+firstname = input("What is your first name?")
+lastname = input("What is your last name?")
+print("Hello,", firstname, lastname)
